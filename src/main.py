c153410c1834799d414ebf337ca236137d60bf2a
Swapnil <swapnilmaddula@gmail.com>
2024-07-09 14:51:47 +0000

    updated main

--- src/main.py
+++ src/main.py
@@ -1,23 +1,25 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import to_date, collect_list, concat_ws
 
-# Initialize Spark session
 spark = SparkSession.builder.appName("Elsevier").getOrCreate()
 
 class Top5Trends:
     
     def __init__(self):
-        pass
+        self.tweets = None
 
     def read_csv(self, filepath):
-        tweets = spark.read.csv(filepath, header=True, inferSchema=True)
-        self = self.tweets
-        return tweets
+        self.tweets = spark.read.csv(filepath, header=True, inferSchema=True)
+        return self.tweets
     
-    def process_tweets(self):
-        
-    
-# Instantiate the class
+    def identify_trending_topics(self):
+        grouped_tweets = self.tweets.withColumn("date", to_date("created_at"))
+        grouped_tweets = grouped_tweets.groupBy("date").agg(concat_ws(",", collect_list("content")).alias("concatenated_content"))
+        grouped_tweets.show()
+        return grouped_tweets
+
+
 top5trends_instance = Top5Trends()
 
-# Read the CSV file
 df = top5trends_instance.read_csv("data/tweet_data.csv")
+grouped_df = top5trends_instance.identify_trending_topics()
