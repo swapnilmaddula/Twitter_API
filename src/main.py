80aa3ffede560e34a4cc872da39682d83ec167ea
Swapnil <swapnilmaddula@gmail.com>
2024-07-09 14:51:35 +0000

    updated main code

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
-#Bird, Steven, Edward Loper and Ewan Klein (2009), Natural Language Processing with Python. O’Reilly Media Inc.
-
 from pyspark.sql import SparkSession
 
+# Initialize Spark session
+spark = SparkSession.builder.appName("Elsevier").getOrCreate()
 
+class Top5Trends:
+    
+    def __init__(self):
+        pass
 
+    def read_csv(self, filepath):
+        tweets = spark.read.csv(filepath, header=True, inferSchema=True)
+        self = self.tweets
+        return tweets
+    
+    def process_tweets(self):
+        
+    
+# Instantiate the class
+top5trends_instance = Top5Trends()
+
+# Read the CSV file
+df = top5trends_instance.read_csv("data/tweet_data.csv")
