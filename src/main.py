11e415b2738326680526de5a46ec24dc6bdde5f5
Swapnil <swapnilmaddula@gmail.com>
2024-07-09 17:28:22 +0000

    updated main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import to_date, collect_list, concat_ws
+from pyspark.sql import functions as F
+from pyspark.sql.functions import to_date, collect_list, concat_ws, udf, StringType
 from stop_words import get_stop_words
 import nltk
 from collections import Counter
@@ -21,24 +22,27 @@ class Top5Trends:
     def obtain_list_of_ignore_words(self, language):
         stop_words = get_stop_words(language)
         return stop_words
-    
     def identify_trending_topics(self):
-        grouped_tweets = self.tweets.withColumn("date", to_date("created_at"))
-        grouped_tweets = grouped_tweets.groupBy("date").agg(concat_ws(",", collect_list("content")).alias("concatenated_content"))
-        grouped_tweets['trending_topics'] = ''
+        grouped_tweets = self.tweets.withColumn("date", F.to_date("created_at"))
+        grouped_tweets = grouped_tweets.groupBy("date").agg(F.concat_ws(",", F.collect_list("content")).alias("concatenated_content"))
+        
         stop_words = self.obtain_list_of_ignore_words("dutch")
-        word_counter = Counter()
-        for row in grouped_tweets:
-            concatenated_content = row['concatenated_content']
-            tokens = nltk.word_tokenize(concatenated_content)
-            filtered_tokens = [i for i in tokens if i not in stop_words]
-            word_counter.update(filtered_tokens)
+        
+        def extract_trending_topics(content):
+            tokens = nltk.word_tokenize(content)
+            filtered_tokens = [token for token in tokens if token.lower() not in stop_words and token.isalpha()]
+            word_counter = Counter(filtered_tokens)
             most_common_words = word_counter.most_common(5)
-            most_common_words_string = ','.join(most_common_words)  
-            grouped_tweets[row['trending_topics']] = most_common_words_string      
+            most_common_words_string = ','.join([word for word, count in most_common_words])
+            return most_common_words_string
+        
+        extract_trending_topics_udf = F.udf(extract_trending_topics, StringType())
+        
+        grouped_tweets = grouped_tweets.withColumn('trending_topics', extract_trending_topics_udf(F.col('concatenated_content')))
+    
         return grouped_tweets
 
-# Usage
+
 top5trends_instance = Top5Trends()
 df = top5trends_instance.read_csv("data/tweet_data.csv")
 grouped_df = top5trends_instance.identify_trending_topics()
