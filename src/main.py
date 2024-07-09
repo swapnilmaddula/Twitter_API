dcb8591a234fc2167d89d1e375d04218229eea91
Swapnil <swapnilmaddula@gmail.com>
2024-07-09 17:23:28 +0000

    modified main

--- src/main.py
+++ src/main.py
@@ -25,27 +25,21 @@ class Top5Trends:
     def identify_trending_topics(self):
         grouped_tweets = self.tweets.withColumn("date", to_date("created_at"))
         grouped_tweets = grouped_tweets.groupBy("date").agg(concat_ws(",", collect_list("content")).alias("concatenated_content"))
-
-        grouped_data = grouped_tweets.collect()
-
+        grouped_tweets['trending_topics'] = ''
         stop_words = self.obtain_list_of_ignore_words("dutch")
         word_counter = Counter()
-        for row in grouped_data:
-            date = row['date']
+        for row in grouped_tweets:
             concatenated_content = row['concatenated_content']
             tokens = nltk.word_tokenize(concatenated_content)
             filtered_tokens = [i for i in tokens if i not in stop_words]
             word_counter.update(filtered_tokens)
-            print(filtered_tokens)
-
-        most_common_words = word_counter.most_common(5)
-        print("Top 5 trending words:")
-        for word, count in most_common_words:
-            print(f"{word}: {count}")
-        
+            most_common_words = word_counter.most_common(5)
+            most_common_words_string = ','.join(most_common_words)  
+            grouped_tweets[row['trending_topics']] = most_common_words_string      
         return grouped_tweets
 
 # Usage
 top5trends_instance = Top5Trends()
 df = top5trends_instance.read_csv("data/tweet_data.csv")
 grouped_df = top5trends_instance.identify_trending_topics()
+print(grouped_df)
