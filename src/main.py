ba4448b01ea25f11619cd29fdc19e17366e40106
Swapnil <swapnilmaddula@gmail.com>
2024-07-10 11:00:20 +0000

    updated main, read_source

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.functions import to_date, collect_list, concat_ws, udf, StringType
-from stop_words import get_stop_words
 import nltk
 from collections import Counter
 
@@ -9,28 +8,31 @@ spark = SparkSession.builder.appName("Elsevier").getOrCreate()
 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('stopwords')
 
 class Top5Trends:
     
-    def __init__(self):
+    def __init__(self, filepath):
+        self.filepath = filepath
         self.tweets = None
 
-    def read_csv(self, filepath):
-        self.tweets = spark.read.csv(filepath, header=True, inferSchema=True)
-        return self.tweets
+    def read_csv(self):
+        tweet_data = spark.read.csv(self.filepath, header=True, inferSchema=True)
+        return tweet_data
     
     def obtain_list_of_ignore_words(self, language):
-        stop_words = get_stop_words(language)
+        stop_words = nltk.corpus.stopwords.words(language)
         return stop_words
+    
     def identify_trending_topics(self):
-        grouped_tweets = self.tweets.withColumn("date", F.to_date("created_at"))
+        tweet_data = self.read_csv()
+        grouped_tweets = tweet_data.withColumn("date", F.to_date("created_at"))
         grouped_tweets = grouped_tweets.groupBy("date").agg(F.concat_ws(",", F.collect_list("content")).alias("concatenated_content"))
-        
         stop_words = self.obtain_list_of_ignore_words("dutch")
         
         def extract_trending_topics(content):
             tokens = nltk.word_tokenize(content)
-            filtered_tokens = [token for token in tokens if token.lower() not in stop_words and token.isalpha()]
+            filtered_tokens = [w for w in tokens if not w.lower() in stop_words]
             word_counter = Counter(filtered_tokens)
             most_common_words = word_counter.most_common(5)
             most_common_words_string = ','.join([word for word, count in most_common_words])
@@ -39,11 +41,8 @@ class Top5Trends:
         extract_trending_topics_udf = F.udf(extract_trending_topics, StringType())
         
         grouped_tweets = grouped_tweets.withColumn('trending_topics', extract_trending_topics_udf(F.col('concatenated_content')))
-    
         return grouped_tweets
 
 
-top5trends_instance = Top5Trends()
-df = top5trends_instance.read_csv("data/tweet_data.csv")
+top5trends_instance = Top5Trends("data/silver/tweet_data/part-00000-5334d6c2-d037-4d69-a017-aed98fc2790e-c000.csv")
 grouped_df = top5trends_instance.identify_trending_topics()
-print(grouped_df)
--- src/read_source_data.py
+++ src/read_source_data.py
@@ -1,37 +1,59 @@
 import json
-import csv
 from datetime import datetime
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType
 
-def read_json_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        data = file.readlines()
-    return data
+spark = SparkSession.builder.appName("Elsevier").getOrCreate()
 
-def parse_json_objects(json_lines):
-    rows = []
-    for line in json_lines:
+class LoadTweetData:
+
+    def __init__(self):
+        self.data = []
+        self.new_data = []
+
+    def read_json_file(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = file.readlines()
+        self.data = data
+        return data
+
+    def parse_json_objects(self):
+        json_lines = self.data
+        rows = []
+        for line in json_lines:
+            try:
+                tweet = json.loads(line.strip())
+                created_at_raw = tweet['interaction']['created_at']
+                created_at = datetime.strptime(created_at_raw, '%a, %d %b %Y %H:%M:%S +0000').isoformat()
+                content = tweet['interaction']['content']
+                tweet_id = tweet['twitter']['id']
+                rows.append([created_at, content, tweet_id])
+            except json.JSONDecodeError:
+                print(f"Error decoding JSON: {line}")
+            except KeyError as e:
+                print(f"Missing key {e} in JSON: {line}")
+            except ValueError as e:
+                print(f"Error parsing date: {created_at_raw} in JSON: {line}")
+        self.new_data = rows
+        return rows
+
+    def incremental_load(self, file_path="data/tweet_data.csv"):
         try:
-            tweet = json.loads(line.strip())
-            created_at_raw = tweet['interaction']['created_at']
-            created_at = datetime.strptime(created_at_raw, '%a, %d %b %Y %H:%M:%S +0000').isoformat()
-            content = tweet['interaction']['content']
-            rows.append([created_at, content])
-        except json.JSONDecodeError:
-            print(f"Error decoding JSON: {line}")
-        except KeyError as e:
-            print(f"Missing key {e} in JSON: {line}")
-        except ValueError as e:
-            print(f"Error parsing date: {created_at_raw} in JSON: {line}")
-    return rows
-
-def write_to_csv(file_path, rows):
-    with open(file_path, 'w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(['created_at', 'content'])
-        writer.writerows(rows)
-input_file_path = 'data/dataset1.json' 
-output_file_path = 'data/tweet_data.csv' 
-
-json_lines = read_json_file(input_file_path)
-rows = parse_json_objects(json_lines)
-write_to_csv(output_file_path, rows)
+            schema = StructType([
+                StructField("created_at", StringType(), True),
+                StructField("content", StringType(), True),
+                StructField("tweet_id", StringType(), True)
+            ])
+            source_data = spark.read.csv(file_path, header=True, schema=schema)
+        except Exception as e:
+            print(f"Error reading source data: {e}")
+            source_data = spark.createDataFrame([], schema)
+        
+        new_data = spark.createDataFrame(self.new_data, schema)
+        
+        if not source_data.rdd.isEmpty():
+            source_data_ids = source_data.select("tweet_id").distinct()
+            new_data = new_data.join(source_data_ids, on="tweet_id", how="left_anti")
+        
+        updated_tweet_data = source_data.union(new_data)
+        updated_tweet_data.coalesce(1).write.csv(file_path, header=True, mode="overwrite")
