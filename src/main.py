29c0911c192110cd87090a22726e9c30945b289d
Swapnil <swapnilmaddula@gmail.com>
2024-07-11 07:35:01 +0000

    updated main

--- src/main.py
+++ src/main.py
@@ -5,9 +5,8 @@ def main(source_file_path, silver_path, gold_path):
     load_tweet_data = read_source_data.LoadTweetData(file_path_source='data/source_data/dataset1.json', folder_path_silver= 'data/silver/tweet_data' )
     load_tweet_data.incremental_load()
 
-    file_path = "data/silver/tweet_data/*.csv"
-    top5trends_instance = identify_trending_topics.Top5Trends(file_path)
-    grouped_df = top5trends_instance.identify_trending_topics()
+    top5trends = identify_trending_topics.Top5Trends(filepath_silver= "data/silver/tweet_data/*.csv", folderpath_gold = "data/gold/top5trends")
+    grouped_df = top5trends.identify_trending_topics()
     grouped_df.show()
 
 if __name__ == "__main__":
